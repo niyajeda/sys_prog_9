ef4c2ef78fe1b0db42b3b75b611d9934255dd05a
daniel <daniel.schroeder7@yahoo.de>
2014-12-09 12:01:51 +0100

    Aufgabe 1a) und 1b) in neuer Ordnerstruktur

---
+++ Aufgabe28/helloworld-a.asm
@@ -0,0 +1,21 @@
+.section .data #Datenteil des Programms
+
+str: 	.ascii "Hello World! \n" #Die ASCII-Datenkette "Hello World!"
+	strlen = . - str #Die Laenge der Zeichenkette
+
+num: 	.long 1337 #long Variable mit Wert 1337
+
+.section .text  
+
+.global _start #Label, das auch fuer den Linker sichtbar ist
+
+_start: #Label fuer das Hauptprogramm
+	movl	$4, %eax #Konstante 4 in Register %eax
+	movl	$1, %ebx #Konstante 1 in Register %ebx
+	movl	$str, %ecx #String in Register %ecx
+	movl	$strlen, %edx #Laenge des Strings in Register %edx
+	int $0x80 #Syscall - da %eax = 4 -> sys_write mit Parameter %ecx (Ausgabestring) und %edx (Laenge des Strings) 
+
+	movl	$1, %eax #Konstante 1 in Register %eax
+	movl	$0, %ebx #Konstante 0 in Register %ebx
+	int $0x80 #Syscall - da 1 in Register %eax -> sys_exit (Programm beenden)
